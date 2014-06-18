fe00fe4b5247903fd5b4ac5ccb505f1de00f4a69
Faiyam Rahman <frahman305@gmail.com>
2014-06-18 11:59:56 -0400

    refactored test code to use private function __assure_bot_setup

--- tests/testBot.py
+++ tests/testBot.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from btsReal.bot import Bot
 from btsReal.filepath import Filepath
+
 class TestBot(unittest.TestCase):
     def setUp(self):
         ## get usernames and passwords for test bots
@@ -60,75 +61,99 @@ class TestBot(unittest.TestCase):
     # @unittest.skip("Not Focus")
     def test__reset_selections(self):
         ## Case 1: Empty Bot -- no selections before AND after func call
-        print "Assure eBot has no selections"
-        self.eBot._get_make_picks_page()
-        answer = ''
-        while answer != 'yes':
-            print "***** Set eBot to no selections then type yes *****"
-            answer = raw_input()
-        # test function
-        # should be no remove buttons before resetting selection
+        self.__assure_bot_setup(self.eBot)
         removeButtonsRaw = self.eBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
+        self.assertEqual(removeButtons, []) 
 
-        self.assertEqual(removeButtons, []) # reset selections
-
+        self.eBot._reset_selections() # reset selections
         # should still be no remove Buttons after _reset_selections
         removeButtonsRaw = self.eBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
-        self.eBot._reset_selections()
         self.assertEqual(removeButtons, [])
 
         ## Case 2: Single Bot -- one selection before, no selections after func call
         self.sBot = Bot(str(self.sBotVals.Email.item()), 
                         str(self.sBotVals.MLBPassword.item()))
-        print "***** Assure sBot has one selections *****"
-        self.sBot._get_make_picks_page()
-        answer = ''
-        while answer != 'yes':
-            print "Set sBot to one selections then type yes"
-            answer = raw_input()
-        # test function
-        # There should be one remove button before we reset selections
+        self.__assure_bot_setup(self.sBot)
         removeButtonsRaw = self.sBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
         self.assertEqual(len(removeButtons), 1)
 
         self.sBot._reset_selections() # reset the selection
-
         # should now be zero removeButtons
         removeButtonsRaw = self.sBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
+        self.assertEqual(removeButtons, [])
 
         ## Case 3: Double (down) Bot -- two selections before, one selection after func call
         self.dBot = Bot(str(self.dBotVals.Email.item()), 
                         str(self.dBotVals.MLBPassword.item()))
-        print "***** Assure dBot has two selections *****"
-        self.dBot._get_make_picks_page()
-        answer = ''
-        while answer != 'yes':
-            print "Set dBot to two selections then type yes"
-            answer = raw_input()
-        # test function
-        # should be two remove buttons before we reset selections
+        self.__assure_bot_setup(self.dBot)
         removeButtonsRaw = self.dBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
         self.assertEqual(len(removeButtons), 2)
 
         self.dBot._reset_selections() # reset selections
-
         # should be no remove buttons after we reset selections
         removeButtonsRaw = self.dBot.browser.find_elements_by_class_name('remove-action')
         removeButtons = [elem for elem in removeButtonsRaw 
             if elem.get_attribute('class') == 'remove-action player-row']
         self.assertEqual(removeButtons, [])
 
+    def __assure_bot_setup(self, bot):
+        """
+        bot -> None
 
+        Helper function to make sure that when they are used, eBot has no
+        selections, sBot has 1 selection and dBot has two selections
+        """
+        if bot == self.eBot:
+            botString = "eBot"
+            selString = '0'
+        elif bot == self.sBot:
+            botString = "sBot"
+            selString = '1'
+        elif bot == self.dBot:
+            botString = "dBot"
+            selString = '2'
+        else:
+            raise Exception("Can only pass one of eBot, sBot or dBot to" + \
+                "_assure_bot_setup")
+        
+        print "***** Assure {0} has {1} selection(s) *****".format(
+                 botString, selString)
+        bot._get_make_picks_page()
+        answer = ''
+        while answer != 'yes':
+            print "Set {0} to {1} selection(s) then type yes".format(
+                     botString, selString)
+            answer = raw_input()
 
+    def test_choose_player(self):
+        ## Case 1: Single Down
+            # Case 1.1: Ebot-> single Down
+
+            ## Assure eBot is empty, then assign him some
+            ## random active player, then check that that
+            ## active player and only that active player is assigned
+            ## to him
+
+            # Case 1.2: sBot -> singleDown
+
+            # Case 1.3: dBot -> single Down
+
+        ## Case 2: Double Down
+            # Case 2.1: Ebot -> double Down
+
+            # Case 2.2: sBot -> double Down
+
+            # Case 2.3: dBot -> double Down
+        self.assertTrue(False)
 
 
