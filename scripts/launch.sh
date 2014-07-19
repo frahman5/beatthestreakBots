d96b35a8c6c5b0eab6decb70cd661f041b999405
Faiyam Rahman <frahman305@gmail.com>
2014-07-19 14:34:44 -0400

    updated launch.sh to run all operations (besides SSHing in) in parallel and created functions to replace explicitly writing everything out

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -17,59 +17,66 @@ tags=--tags="http-server,https-server"
 autodelete=--auto_delete_boot_disk="true"
 
 ## Home Computer parameters
-minionFiles=/users/faiyamrahman/programming/Python/beatthestreakBots/accountFiles/minionAccountFiles/
 finalMinionDir=/users/faiyamrahman/Desktop/final/minions/
-sN5vMN1Excel="sN=5,vMN=1.xlsx"
-sN5vMN2Excel="sN=5,vMN=2.xlsx"
 
-## Disk names
-disk5=--disk="sn5,deviceName=sn5,mode=READ_WRITE,boot"
-disk6=--disk="sn6,deviceName=sn6,mode=READ_WRITE,boot"
-disk7=--disk="sn7,deviceName=sn7,mode=READ_WRITE,boot"
-disk8=--disk="sn8,deviceName=sn8,mode=READ_WRITE,boot"
-disk9=--disk="sn9,deviceName=sn9,mode=READ_WRITE,boot"
-disk10=--disk="sn10,deviceName=sn10,mode=READ_WRITE,boot"
-disk11=--disk="sn11,deviceName=sn11,mode=READ_WRITE,boot"
-disk12=--disk="sn12,deviceName=sn12,mode=READ_WRITE,boot"
-disk13=--disk="sn13,deviceName=sn13,mode=READ_WRITE,boot"
-disk14=--disk="sn14,deviceName=sn14,mode=READ_WRITE,boot"
-disk15=--disk="sn15,deviceName=sn15,mode=READ_WRITE,boot"
-disk16=--disk="sn16,deviceName=sn16,mode=READ_WRITE,boot"
-disk17=--disk="sn17,deviceName=sn17,mode=READ_WRITE,boot"
+function makeDisk {
+    # strategyNumber -> None
+    # Given e.g input 5, makes disk sn5 from snapshot
+    gcutil $sv $p adddisk 'sn'$1 $zone $snapshot $disktype
+}
 
-# ## Spin up instances in series
-#     # First the disks
-# for disk in sn5, sn6, sn7, sn8, sn9, sn10, sn11, sn12, sn13, sn14, sn15, sn16, sn17
-# do
-#     gcutil $sv $p adddisk $disk $zone $snapshot $disktype
-# done
+function makeInstance {
+    # strategyNumber -> None
+    # Given e.g input 5, makes instance sn5. Assumes disk sn5 has already been
+    # created
+    disk='--disk=sn'$1',deviceName=sn'$1',mode=READ_WRITE,boot'
+    gcutil $sv $p addinstance 'sn'$1 $zone $machinetype $network $ip $service $disk $autodelete
+}
 
-## For each strategy, make the disk, then make the instance
-for pair in "sn5 $disk5" "sn6 $disk6" "sn7 $disk7" "sn8 $disk8" "sn9 $disk9" \
-            "sn10 $disk10" "sn11 $disk11" "sn12 $disk12" "sn13 $disk13" \
-            "sn14 $disk14"  "sn15 $disk15" "sn16 $disk16" "sn17 $disk17"
-do
-    set -- $pair
-       # make the disk
-    gcutil $sv $p adddisk $1 $zone $snapshot $disktype
-       # make the instance
-    gcutil $sv $p addinstance $1 $zone $machinetype $network $ip $service $2 $autodelete
-done
+function pushFiles {
+    # strategyNumber -> None
+    # Pushes a launch script and minion account files to the instance
+    # for the given strategy
 
-## Then give them the necessary files, SSH in, and launch scripts
-for strategy in 5 6 7 8 9 10 11 12 13 14 15 16 17
-do
-    minion1='sN='$strategy',vMN=1.xlsx'
-    minion2='sN='$strategy',vMN=2.xlsx'
-    instance='sn'$strategy
-        # push the launch script
+    # Create the necessary variables
+    minion1='sN='$1',vMN=1.xlsx'
+    minion2='sN='$1',vMN=2.xlsx'
+    instance='sn'$1
+
+    # push the launch script
     gcutil push $p $instance go.sh /home/faiyamrahman 
-        # push the minion account files
+
+    # push the minion account files
     gcutil push $p $instance $finalMinionDir$minion1 $googleMinionDir 
     gcutil push $p $instance $finalMinionDir$minion2 $googleMinionDir 
-        # ssh in and launch both strategies. REMEMBER:
-            # Launch a screen, run go.sh for vmn 1, then another screen
-            # then go.sh for vmn2, then ctrl+a, d to detach from the screen,
-            # then exit the instance
-    gcutil $sv $p ssh $zone $instance
+}
+
+## Create all the disks in parallel
+    # wait makes shell wait for all jobs running background to finish
+makeDisk 5 & makeDisk 6 & makeDisk 7 & makeDisk 8 & makeDisk 9 & makeDisk 10 & \
+makeDisk 11 & makeDisk 12 & makeDisk 13 & makeDisk 14 & makeDisk 15 & makeDisk 16 \
+& makeDisk 17 & wait 
+echo "**************** DISKS CREATED! ****************"
+
+## Create all the instances in parallel
+makeInstance 5 & makeInstance 6 & makeInstance 7 & makeInstance 8 & \
+makeInstance 9 & makeInstance 10 & makeInstance 11 & makeInstance 12 & \
+makeInstance 13 & makeInstance 14 & makeInstance 15 & makeInstance 16 & \
+makeInstance 17 & wait 
+echo "**************** INSTANCES CREATED! ****************"
+
+# Sleep for a bit to make sure instances are ready to receive files
+echo "\n-->Sleeping for 60 seconds to allow instances to be ready to receive files"
+sleep 60 
+
+## Push all the files in parallel
+pushFiles 5 & pushFiles 6 & pushFiles 7 & pushFiles 8 & pushFiles 9 & pushFiles 10 & \
+pushFiles 11 & pushFiles 12 & pushFiles 13 & pushFiles 14 & pushFiles 15 & pushFiles 16 & \
+pushFiles 17 & wait 
+echo "**************** FILES PUSHED! ****************"
+
+## SSH into each one and launch your bots!
+for strategy in 5 6 7 8 9 10 11 12 13 14 15 16 17
+do
+    gcutil $sv $p ssh $zone 'sn'$strategy
 done
