f2175e725f167cdb56e18b88cc4967df9adfb1b7
Faiyam Rahman <frahman305@gmail.com>
2014-07-28 18:12:13 -0400

    tried to fix eligiblePlayers issue

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,5 @@
 ## Script to delete instances after they've been used
 
 # Delete all the instances, updating first
-./update.sh $1
 gcutil --project='faiyam-bts-1' deleteinstance sn5 sn6 sn7 sn8 sn9 sn10 sn11 \
                                                sn12 sn13 sn14 sn15 sn16 sn17
